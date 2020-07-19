61df4cf7f62062623b269e0c08fb57a6095e9c37
Aanisha Bhattacharyya <43934116+Aanisha@users.noreply.github.com>
2020-07-19 07:34:57 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,10 +1,11 @@
-import cv2
+
 import numpy as np
 import math
 import os
 
 from tensorflow.keras.models import model_from_json
 from skimage.measure import compare_ssim as ssim
+import cv2
 from matplotlib import pyplot as plt
 from prepare_images import psnr,mse,compare_images,prepare_images
 # define necessary image processing functions
@@ -79,51 +80,3 @@ def predict(image,img_name):
 
 
 
-# print all scores for all images
-#print('Degraded Image: \nPSNR: {}\nMSE: {}\nSSIM: {}\n'.format(scores[0][0], scores[0][1], scores[0][2]))
-"""print('Reconstructed Image: \nPSNR: {}\nMSE: {}\nSSIM: {}\n'.format(scores[1][0], scores[1][1], scores[1][2]))
-
-
-# display images as subplots
-fig, axs = plt.subplots(1, 3, figsize=(20, 8))
-axs[0].imshow(cv2.cvtColor(ref, cv2.COLOR_BGR2RGB))
-axs[0].set_title('Original')
-axs[1].imshow(cv2.cvtColor(degraded, cv2.COLOR_BGR2RGB))
-axs[1].set_title('Degraded')
-axs[2].imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
-axs[2].set_title('SRCNN')
-
-# remove the x and y ticks
-for ax in axs:
-    ax.set_xticks([])
-    ax.set_yticks([])
-
-
-"""
-"""for file in os.listdir('images'):
-    
-    # perform super-resolution
-    ref, degraded, output, scores = predict('images/{}'.format(file))
-    
-    # display images as subplots
-    fig, axs = plt.subplots(1, 3, figsize=(20, 8))
-    axs[0].imshow(cv2.cvtColor(ref, cv2.COLOR_BGR2RGB))
-    axs[0].set_title('Original')
-    axs[1].imshow(cv2.cvtColor(degraded, cv2.COLOR_BGR2RGB))
-    axs[1].set_title('Degraded')
-    axs[1].set(xlabel = 'PSNR: {}\nMSE: {} \nSSIM: {}'.format(scores[0][0], scores[0][1], scores[0][2]))
-    axs[2].imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
-    axs[2].set_title('SRCNN')
-    axs[2].set(xlabel = 'PSNR: {} \nMSE: {} \nSSIM: {}'.format(scores[1][0], scores[1][1], scores[1][2]))
-
-    # remove the x and y ticks
-    for ax in axs:
-        ax.set_xticks([])
-        ax.set_yticks([])
-      
-    print('Saving {}'.format(file))
-    fig.savefig('output/{}.png'.format(os.path.splitext(file)[0])) 
-    plt.close()
-"""
-
-#https://storage.googleapis.com/tensorflow/windows/cpu/tensorflow_cpu-2.2.0-cp37-cp37m-win_amd64.whl
